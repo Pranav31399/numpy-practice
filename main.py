847531079dab04c56ff236f16ec43cbbc2ec0bdb
Pranav Dewangan <pranavdewangan3@gmail.com>
2026-02-20 20:29:40 +0530

    updated slicing part

--- main.py
+++ main.py
@@ -43,3 +43,45 @@ print(array[0][0][0])
 print(array[1,1,1])
 
 # multidimentional indexing is faster
+
+# string concatenation
+word=array[0,0,0]+array[1,0,0]
+print(word)
+
+# slicing
+array=np.array([
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11,12],
+    [13,14,15,16]
+])
+
+# array[start:end:step]
+# end is exlusive
+# step is 1 by default
+print(array[0:3])
+
+# start from 1 till the end
+print(array[1:])
+
+# select all with step 2
+print(array[::2])
+
+# reverse
+print(array[::-1])
+
+# column selection
+# select all row and column 0
+print(array[:, 0])
+
+# select all row with column 2 from the end
+print(array[:,-2])
+
+# print all 3 columns
+print(array[:,0:3])
+
+# every second column
+print(array[:,::2])
+
+# selecting row and column
+print(array[0:2,0:2])
