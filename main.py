56e3d248a5cff3cd5a3bcc028ed877b0b8662dce
Pranav Dewangan <pranavdewangan3@gmail.com>
2026-02-20 20:57:44 +0530

    updated scaler arithmetic, broadcasting, aggregate functions

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import numpy as np
 
 print(np.__version__)
 
+#######################################################################################################################
+# basics #
+#######################################################################################################################
 # to create the numpy array
 array=np.array([1,2,3,4])
 print(array)
@@ -36,6 +39,9 @@ print(array.ndim)
 # (depth, no of rows, no of columns)
 print(array.shape)
 
+#######################################################################################################################
+# indexing #
+#######################################################################################################################
 # chain indexing
 print(array[0][0][0])
 
@@ -48,7 +54,9 @@ print(array[1,1,1])
 word=array[0,0,0]+array[1,0,0]
 print(word)
 
-# slicing
+#######################################################################################################################
+# slicing #
+#######################################################################################################################
 array=np.array([
     [1,2,3,4],
     [5,6,7,8],
@@ -85,3 +93,109 @@ print(array[:,::2])
 
 # selecting row and column
 print(array[0:2,0:2])
+
+#######################################################################################################################
+# scaler arithmetic #
+#######################################################################################################################
+
+array=np.array([1,2,3])
+
+print(array+1)
+print(array-2)
+print(array*3)
+print(array/4)
+print(array**5)
+
+# vectroized math functions
+array=np.array([1.01,2.5,3.99])
+
+print(np.sqrt(array))
+print(np.round(array))
+print(np.ceil(array))
+print(np.pi)
+
+# area of a cicle
+radii=np.array([1,2,3])
+print(np.pi*radii**2)
+
+# element wise arithmetic
+array1=np.array([1,2,3])
+array2=np.array([4,5,6])
+
+print(array1+array2)
+print(array1-array2)
+print(array1*array2)
+print(array1/array2)
+print(array1**array2)
+
+# comparision operations
+scores=np.array([91,55,100,73,82,64])
+
+# returns a boolean array
+print(scores==100)
+print(scores>=60)
+
+scores[scores<60]=0
+print(scores)
+
+#######################################################################################################################
+# broadasting #
+#######################################################################################################################
+
+# Broadcasting allows numpy to perferm operations on arrays
+# with different shapes by virtually expanding dimensions
+# so they match the larger array's shape
+# rules
+# the dimensions have the same size
+# or
+# one of the dimensions has a size of 1
+
+# a(4*3) + b(1*3) = result(4*3)
+
+array1=np.array([
+    [1,2,3,4]
+])
+
+array2=np.array([
+    [1],
+    [2],
+    [3],
+    [4]
+])
+
+print(array1.shape)
+print(array2.shape)
+
+# braodcast is possible (1,4), (4,1)
+print(array1*array2)
+
+# we cannot broadcast for (2,4), (4,1)
+
+#######################################################################################################################
+# aggregate functions #
+#######################################################################################################################
+
+# summarise data and typically return a single value
+array=np.array([
+    [1,2,3,4,5],
+    [6,7,8,9,10]
+])
+
+print(np.sum(array))
+print(np.mean(array))
+print(np.std(array))
+print(np.var(array))
+print(np.min(array))
+print(np.max(array))
+# pos of min val
+print(np.argmin(array))
+print(np.argmax(array))
+# sum all columns
+print(np.sum(array,axis=0))
+# sum all rows
+print(np.sum(array,axis=1))
+
+#######################################################################################################################
+# filtering #
+#######################################################################################################################
+
